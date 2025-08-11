d62bbe87eb794e8bdb545d269c36f21d4dadc5a8
Aastha <aastha5450a@gmail.com>
2025-08-11 15:33:25 +0530

    Signup page

--- frontend/src/Signup.tsx
+++ frontend/src/Signup.tsx
@@ -1,4 +1,4 @@
-function Login(){
+function Signup(){
 return (
     <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -8,7 +8,7 @@ return (
             className="mx-auto h-10 w-auto"
           />
           <h2 className="mt-10 text-center text-2xl font-bold tracking-tight text-gray-900">
-            Sign in to your account
+            Sign up for a new account
           </h2>
         </div>
 
@@ -66,21 +66,15 @@ return (
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Sign in
+                Sign Up
               </button>
             </div>
           </form>
 
-          <p className="mt-10 text-center text-sm/6 text-gray-500">
-            Not a member?{' '}
-            <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-              Create account
-            </a>
-          </p>
         </div>
       </div>
                 );
 }
 
 
-export default Login
+export default Signup
